950623a021f0c8cbd8c0d47e3f53bf17adf085df
Mark Waite <mark@waite.de>
2012-10-27 20:42:02 +1000

    WHAT: Added first test cases for User Controller Login

--- app/Test/Case/Controller/UsersControllerTest.php
+++ app/Test/Case/Controller/UsersControllerTest.php
@@ -29,13 +29,13 @@ class TestUsersController extends UsersController {
  * UsersController Test Case
  *
  */
-class UsersControllerTestCase extends CakeTestCase {
+class UsersControllerTest extends ControllerTestCase {
 /**
  * Fixtures
  *
  * @var array
  */
-	public $fixtures = array('app.user', 'app.tenant', 'app.roletype', 'app.profile', 'app.mentor_extra_info', 'app.mentee_extra_info', 'app.user_away_date');
+	public $fixtures = array('app.user', 'app.tenant', 'app.roletype', 'app.profile', 'app.mentor_extra_info', 'app.mentee_extra_info', 'app.user_away_date', 'app.user_address');
 
 /**
  * setUp method
@@ -64,39 +64,59 @@ class UsersControllerTestCase extends CakeTestCase {
  *
  * @return void
  */
-	public function testIndex() {
-
-	}
+	// public function testIndex() {
+            
+	// }
 /**
  * testView method
  *
  * @return void
  */
-	public function testView() {
+	//public function testView() {
 
-	}
+	//}
 /**
  * testAdd method
  *
  * @return void
  */
-	public function testAdd() {
+	//public function testAdd() {
 
-	}
+	//}
 /**
  * testEdit method
  *
  * @return void
  */
-	public function testEdit() {
+	//public function testEdit() {
 
-	}
+	//}
 /**
  * testDelete method
  *
  * @return void
  */
-	public function testDelete() {
+	//public function testDelete() {
 
-	}
+	//}
+        
+        public function testLogin() {
+             $data = array(
+                'User' => array(
+                    'email' => 'superadmin@mentor.com',
+                    'password' => 'password'
+                )
+            );
+            // Call up the login page (located at root URL!)
+            $result = $this->testAction('/', array( 'return' => 'view', 'method' => 'get'));
+            // Check that the e-mail input field is there
+            $this->assertContains('<input name="data[User][email]" maxlength="255" type="text" id="UserEmail"/>', $this->view);
+            // Check that the password input field is there
+            $this->assertContains('<input name="data[User][password]" type="password" id="UserPassword"/>', $this->view);
+            // Check that the submit button is there
+            $this->assertContains('<button type="submit">Login</button>', $this->view);
+            
+            $result = $this->testAction('/', array( 'data' => $data, 'return' => 'view', 'method' => 'post'));
+            debug($this->view);
+        }
 }
